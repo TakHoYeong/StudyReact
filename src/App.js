d5837b61c6e877a9f58105a1937f11bc0483146f
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2023-09-15 12:44:13 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* 주석을 박스로 감싸줘야한다. */}
+        <h1
+        // 주석 작성성
+        >
+          리액트
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
